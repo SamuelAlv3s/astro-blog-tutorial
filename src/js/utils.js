5f153cd6d9a75b77b9f5600982d8ebf8e84ee24e
Samuel <samuelalves681@gmail>
2023-06-20 21:32:33 -0300

    markdown layouts/routing

---
+++ src/js/utils.js
@@ -0,0 +1,18 @@
+export function slugify(text) {
+  return text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(/[^\w\-]+/g, "") // Remove all non-word chars
+    .replace(/\-\-+/g, "-") // Replace multiple - with single -
+    .replace(/^-+/, "") // Trim - from start of text
+    .replace(/-+$/, ""); // Trim - from end of text
+}
+
+export function formatDate(date) {
+  return new Date(date).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+}
