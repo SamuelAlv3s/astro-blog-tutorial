49a1b6c810bd0e7ed605bb27a0b654e3ce07925b
Samuel <samuelalves681@gmail>
2023-06-20 22:28:39 -0300

    filter/sort posts

--- src/js/utils.js
+++ src/js/utils.js
@@ -16,3 +16,46 @@ export function formatDate(date) {
     day: "numeric",
   });
 }
+
+export function formatBlogPosts(
+  posts,
+  {
+    filterOutDrafts = true,
+    filterOutFuturePosts = true,
+    sortByDate = true,
+    limit = undefined,
+  } = {}
+) {
+  const filteredPosts = posts.reduce((acc, post) => {
+    const { date, draft } = post.frontmatter;
+
+    if (filterOutDrafts && draft) {
+      return acc;
+    }
+
+    if (filterOutFuturePosts && new Date(date) > new Date()) {
+      return acc;
+    }
+
+    acc.push(post);
+
+    return acc;
+  }, []);
+
+  if (sortByDate) {
+    filteredPosts.sort((a, b) => {
+      const aDate = new Date(a.frontmatter.date);
+      const bDate = new Date(b.frontmatter.date);
+
+      return bDate - aDate;
+    });
+  } else {
+    filteredPosts.sort(() => Math.random() - 0.5);
+  }
+
+  if (typeof limit === "number") {
+    return filteredPosts.slice(0, limit);
+  }
+
+  return filteredPosts;
+}
