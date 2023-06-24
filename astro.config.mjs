94243815cec02c899495fe20d7da3cf96aec13b6
Samuel <samuelalves681@gmail>
2023-06-24 14:02:45 -0300

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
-
 import image from "@astrojs/image";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://astro-blog-tutorial.vercel.app/",
   integrations: [
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
+    sitemap(),
   ],
 });
